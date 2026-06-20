d3e375b7dd461825525757cc138d466efd1296c8
Maxi <arora.anubhav93@gmail.com>
2026-06-20 17:22:45 +0530

    Added module and project level build script files and settings gradle file

---
+++ sample-consumer/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    includeBuild("..")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sample-consumer"
+include(":app")
